7c6b813f96db8880d65e0d4d31e8e86e6411139a
Christoph Burandt <christoph.burandt@pionix.de>
2026-05-28 22:58:01 +0200

    Fix generation of 'latest'
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>

--- cmake/trailbook/add-trailbook.cmake
+++ cmake/trailbook/add-trailbook.cmake
@@ -258,6 +258,8 @@ macro(_add_trailbook_create_metadata_file_command)
             ${STEM_FILES_BUILD_DIR}
             ${CHECK_DONE_FILE_SETUP_BUILD_DIRECTORY}
             ${CURRENT_INSTANCE_INFO_JSON}
+            ${CHECK_DONE_FILE_REPLACE_LATEST}
+            trailbook_${args_NAME}_replace_latest_folder
             trailbook_${args_NAME}_stage_prepare_sphinx_source_before
             $<TARGET_PROPERTY:trailbook_${args_NAME},ADDITIONAL_DEPS_STAGE_PREPARE_SPHINX_SOURCE_BEFORE>
         COMMENT
@@ -339,6 +341,10 @@ macro(_add_trailbook_replace_latest_command)
             trailbook_${args_NAME}_stage_postprocess_sphinx_before
             $<TARGET_PROPERTY:trailbook_${args_NAME},ADDITIONAL_DEPS_STAGE_POSTPROCESS_SPHINX_BEFORE>
             ${CHECK_DONE_FILE_SPHINX_BUILD_COMMAND}
+            # Wait for info.json to be placed inside the instance build dir
+            # before snapshotting it as 'latest', otherwise the snapshot is
+            # missing info.json.
+            ${CHECK_DONE_FILE_COPY_INSTANCE_INFO}
         COMMENT
             "Trailbook: ${args_NAME} - Replacing 'latest' copy with copy of current instance"
         COMMAND
@@ -349,6 +355,13 @@ macro(_add_trailbook_replace_latest_command)
             ${TRAILBOOK_BUILD_DIRECTORY}/latest
         COMMAND
             ${CMAKE_COMMAND} -E touch ${CHECK_DONE_FILE_REPLACE_LATEST}
+    )
+    add_custom_target(
+        trailbook_${args_NAME}_replace_latest_folder
+        DEPENDS
+            ${CHECK_DONE_FILE_REPLACE_LATEST}
+        COMMENT
+            "Trailbook: ${args_NAME} - replace latest release folder"
     )
 endmacro()
 
@@ -450,6 +463,7 @@ macro(_add_trailbook_copy_versions_index_command)
             $<TARGET_PROPERTY:trailbook_${args_NAME},ADDITIONAL_DEPS_STAGE_POSTPROCESS_SPHINX_BEFORE>
             ${CHECK_DONE_FILE_SETUP_BUILD_DIRECTORY}
             ${TRAILBOOK_VERSIONS_INDEX_SOURCE}
+            trailbook_${args_NAME}_copy_instance_info_file
         COMMENT
             "Trailbook: ${args_NAME} - Installing static versions_index.html in multiversion root directory"
         COMMAND
@@ -515,6 +529,13 @@ macro(_add_trailbook_copy_instance_info_command)
             ${TRAILBOOK_INSTANCE_INFO_FILE}
         COMMAND
             ${CMAKE_COMMAND} -E touch ${CHECK_DONE_FILE_COPY_INSTANCE_INFO}
+    )
+    add_custom_target(
+        trailbook_${args_NAME}_copy_instance_info_file
+        DEPENDS
+            ${CHECK_DONE_FILE_COPY_INSTANCE_INFO}
+        COMMENT
+            "Trailbook: ${args_NAME} - copy instance info file"
     )
 endmacro()
 
@@ -738,14 +759,14 @@ function(add_trailbook)
             $<TARGET_PROPERTY:trailbook_${args_NAME},ADDITIONAL_DEPS_STAGE_POSTPROCESS_SPHINX_BEFORE>
             trailbook_${args_NAME}_stage_build_sphinx_after
     )
-    _add_trailbook_copy_versions_index_command()
-    _add_trailbook_copy_versions_json_command()
-    _add_trailbook_copy_instance_info_command()
     if(TRAILBOOK_INSTANCE_IS_RELEASE)
         _add_trailbook_replace_latest_command()
         _add_trailbook_copy_404_command()
         _add_trailbook_render_redirect_template_command()
     endif()
+    _add_trailbook_copy_instance_info_command()
+    _add_trailbook_copy_versions_index_command()
+    _add_trailbook_copy_versions_json_command()
 
     set(DEPS_STAGE_POSTPROCESS_SPHINX_AFTER
         trailbook_${args_NAME}_stage_postprocess_sphinx_before
